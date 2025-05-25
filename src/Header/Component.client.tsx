f9a012c5216911eee44e672a8b0b0c9deffd0804
bradpreston <bap5393@gmail.com>
2025-05-25 18:34:52 -0400

    Update the Header global to use daisyui

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -7,7 +7,7 @@ import React, { useEffect, useState } from 'react'
 import type { Header } from '@/payload-types'
 
 import { Logo } from '@/components/Logo/Logo'
-import { HeaderNav } from './Nav'
+import { CMSLink } from '@/components/Link'
 
 interface HeaderClientProps {
   data: Header
@@ -29,14 +29,59 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [headerTheme])
 
+  const navItems = data?.navItems || []
+
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
+    <div className="navbar bg-base-100 shadow-sm container mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {' '}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{' '}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {navItems.map(({ link }) => {
+              return (
+                <li key={link.label}>
+                  <CMSLink {...link} className="text-lg" />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
         <Link href="/">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
         </Link>
-        <HeaderNav data={data} />
       </div>
-    </header>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navItems.map(({ link }) => {
+            console.log(link)
+            return (
+              <li key={link.label}>
+                <CMSLink {...link} />
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="navbar-end"></div>
+    </div>
   )
 }
